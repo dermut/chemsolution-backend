541fe67d5771550239ef5f9d9cf506e287cdbaa2
Jimin Kim <ultimate1994@naver.com>
2022-01-22 14:00:59 +0900

    CHEM-1 켐솔루션 프로젝트 시작

---
+++ src/main/java/com/jimin/chemsolution/ChemsolutionApplication.java
@@ -0,0 +1,13 @@
+package com.jimin.chemsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemsolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChemsolutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jimin/chemsolution/ChemsolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimin.chemsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemsolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
