ba6c25d87329864396ba58cd752cd58650c691cc
Jimin Kim <ultimate1994@naver.com>
2022-01-22 23:37:23 +0900

    CHEM-1 켐솔루션 프로젝트 시작 인덱스 페이지 생성

---
+++ src/main/java/com/jimin/index/IndexController.java
@@ -0,0 +1,15 @@
+package com.jimin.index;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        ModelAndView mav = new ModelAndView("/index.html");
+        return mav;
+    }
+}
